9ae0693762d80a0ceccfb8d27e42699f8bb31863
meehog527 <52838950+meehog527@users.noreply.github.com>
2022-05-10 17:44:49 -0400

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-docker run --rm --privileged \
-    -v ~/.docker:/root/.docker \
-    homeassistant/amd64-builder \
-    --all \
-    -r https://github.com/meehog527/hassio-espruinohub.git \
-    -b master \
-    -t espruinohub
