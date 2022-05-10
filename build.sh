95b3eba2eaef207b6d11040a0b421b17510ef429
meehog527 <52838950+meehog527@users.noreply.github.com>
2022-05-10 17:42:24 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 docker run --rm --privileged \
     -v ~/.docker:/root/.docker \
-    homeassistant/armv7-builder \
+    homeassistant/amd64-builder \
     --all \
     -r https://github.com/meehog527/hassio-espruinohub.git \
     -b master \
