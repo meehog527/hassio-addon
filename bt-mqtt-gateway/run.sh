20bb0a7a3b181f87782b5e268f90694145bd0ad0
meehog527 <52838950+meehog527@users.noreply.github.com>
2022-05-13 09:59:06 -0400

    Update run.sh

--- bt-mqtt-gateway/run.sh
+++ bt-mqtt-gateway/run.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/with-contenv bashio
 # ==============================================================================
-# Home Assistant Community Add-on: WireGuard
-# Runs WireGuard
+# Home Assistant Community Add-on: BT-MQTT Gateway
+# Runs BT-MQTT Gateway
 # ==============================================================================
+
 bashio::log.info "Starting BT-MQTT Gateway..."
 bashio::log.info "Installing python packages..."
 sudo pip3 install -r requirements.txt
